e59806c98bf28e8af08f1beefb2c0cf9647d5c37
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-01-23 12:44:00 -0600

    Add files via upload

---
+++ Hola_Mundo.c
@@ -0,0 +1,18 @@
+/*
+				***** HOLA MUNDO *****
+		
+		El siguiente código implementa el hola mundo de la programación a través del
+		lenguaje C.
+		Fecha: 08-Octubre-2021
+		Version: 1.0
+		
+*/
+
+#include <stdio.h>                              // Cabecera estandar de Entrada y Salida de datos
+    
+int main()                                      // Función principal
+{
+	printf("*** HOLA MUNDO ***");               // Imprime texto en consola
+	
+	return 0;                                   // Programa finalizado con éxito
+}   

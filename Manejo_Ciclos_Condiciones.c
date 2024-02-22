4e3e4483baf527a1ee5e3bb54455a86c490d10d3
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-02-22 11:40:41 -0600

    Add files via upload

---
+++ Manejo_Ciclos_Condiciones.c
@@ -0,0 +1,44 @@
+/*
+            ***** CICLOS Y CONDICIONES *****
+
+        TAE 2024
+        Author: Cesar Antonio Miranda Alberto
+        Fecha: 20-Febrero-2024
+        Versión: 1.0
+
+*/
+
+#include <stdio.h>                                                                                  // Biblioteca estandar de entrada y salida de datos
+
+#define TAMANO_VECTOR 5                                                                             // Constante con el tamaño del vector
+
+int main()                                                                                           // Función Principal
+{
+    int numeros[TAMANO_VECTOR];                                                                      // Declaración del vector con respecto a la constante
+    int buffer;                                                                                      // Variable para almacenar temporalmente
+
+    printf("Ingresa los cinco valores del vector: ");
+    fflush(stdin);                                                                                   // Limpieza del buffer de entrada
+    scanf("%i %i %i %i %i",&numeros[0],&numeros[1],&numeros[2],&numeros[3],&numeros[4]);             // Ingreso de datos
+
+    //Metodo bubblesort para evaluar los valores del vector
+    for (int i=0; i<TAMANO_VECTOR; i++)
+    {
+        for(int k=0; k<TAMANO_VECTOR; k++)
+        {
+             if(numeros[k] > numeros[k+1])
+            {
+                buffer = numeros[k];
+                numeros[k]= numeros[k+1];
+                numeros[k+1] = buffer;
+            }
+        }
+    }
+
+    // Ciclo for para mostar los valores del vector con iteraciones respecto al tamaño
+    for(int i=TAMANO_VECTOR;i>0; i--)
+    {
+         printf("%i es el valor del vector en la posicion %i del vector \n", numeros[i],i-1);
+    }
+    return 0;                       // Programa terminado con éxito
+}
